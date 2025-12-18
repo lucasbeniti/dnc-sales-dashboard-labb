2aa726af8ee89d7a2033f761f05f78caff1228ed
Lucas André Beniti Bernardo <lucasbeniti.bernardo@gmail.com>
2025-12-18 16:50:03 -0300

    feat: creating initial routes and cleaning vite files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>LOGIN</h1>} />
+        <Route path="/cadastro" element={<h1>CADASTRO</h1>} />
+        <Route path="/home" element={<h1>HOME</h1>} />
+        <Route path="/leads" element={<h1>LEADS</h1>} />
+        <Route path="/perfil" element={<h1>PERFIL</h1>} />
+      </Routes>
+    </Router>
   )
 }
 
